f7ce85f6b4683dc07985e2ee403cf5bed82282ae
Winston Liu <Wliu1402@gmail.com>
2020-03-10 18:52:10 -0400

    Ditto for the other two files

--- ibcm/simulator.php
+++ ibcm/simulator.php
@@ -7,15 +7,18 @@ $mem = array();
 $filestatus = "";
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<!DOCTYPE html>
+<html lang="en">
  <head>
-  <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+
   <meta name="description" content="Site description" />
   <meta name="keywords" content="IBCM, Itty Bitty Computing Machine" />
+
+  <link rel="stylesheet" type="text/css" href="ibcm.css" />
+
   <title>IBCM Interface: Simulator</title>
-  <link rel="stylesheet" type="text/css" href="ibcm.css" media="screen" />
 
 <?php
 function load_ibcm_file () {
