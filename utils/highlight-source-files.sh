2b6a56b3b2c93575f78da5be7f84237211b8922a
Aaron Bloomfield <aaron@virginia.edu>
2026-04-12 10:39:57 -0400

    Source code accessibility updates

--- utils/highlight-source-files.sh
+++ utils/highlight-source-files.sh
@@ -9,20 +9,10 @@ fi
 # take every path in the repo ending in .c, .cpp, .h, or Makefile, ignoring those under book
 for infile in `find . -type f \( -name '*.c' -or -name '*.cpp' -or\
                                  -name '*.h' -or -name 'Makefile' \) | grep -v 'book'`; do
-    source-highlight --doc $infile --title `basename $infile` --out-format html5 --quiet --gen-version
-    sed -i s/"<html>"/"<html lang='en'>"/ $infile.html
-    sed -i s/"<pre>"/"<main><pre>"/ $infile.html
-    sed -i s_"</pre>"_"</pre></main>"_ $infile.html
-    sed -i s/#FF0000/#EF0000/g $infile.html
-    sed -i s/#009900/#008A00/g $infile.html
+    custom-source-highlight.sh $infile
 done
 
 # for Assembly files, treat them as Assembly and not S
 for infile in `find . -type f -name '*.s' | grep -v 'book'`; do
-    source-highlight --doc $infile --title `basename $infile` --out-format html5 --quiet --gen-version --lang-def 'asm.lang'
-    sed -i s/"<html>"/"<html lang='en'>"/ $infile.html
-    sed -i s/"<pre>"/"<main><pre>"/ $infile.html
-    sed -i s_"</pre>"_"</pre></main>"_ $infile.html
-    sed -i s/#FF0000/#EF0000/g $infile.html
-    sed -i s/#009900/#008A00/g $infile.html
+    custom-source-highlight.sh $infile
 done
